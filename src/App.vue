cda5910232bf3ed03133228f4ccc9520b6460c44
Wang-Gong-github <1762253901@qq.com>
2025-02-17 22:07:22 +0800

    feat: 创建物流企业官网首页，包含企业文化、车型展示、司机团队和视频展示模块

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-config-provider>
+    <Home />
+  </el-config-provider>
+</template>
+
+<script setup>
+import { ElConfigProvider } from 'element-plus'
+import Home from './components/Home.vue'
+</script>
