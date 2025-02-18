d86f793f73588150e3b4715966c5ffb4b1e5a73f
Wang-Gong-github <1762253901@qq.com>
2025-02-18 23:39:02 +0800

    feat: 更新排版

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,12 +8,12 @@ export default {
       {
         name: '重型货车',
         description: '载重量：40吨，适合长途运输',
-        image: '/images/truck1.jpg'
+        image: '/images/logo.jpg'
       },
       {
         name: '厢式货车',
         description: '载重量：15吨，适合城市配送',
-        image: '/images/truck2.jpg'
+        image: '/images/logo.jpg'
       }
     ])
 
@@ -23,19 +23,19 @@ export default {
         name: '张师傅',
         experience: '从业15年',
         description: '安全驾驶里程超过100万公里',
-        image: '/images/driver1.jpg'
+        image: '/images/logo.jpg'
       },
       {
         name: '李师傅',
         experience: '从业10年',
         description: '优秀驾驶员，多次获得表彰',
-        image: '/images/driver2.jpg'
+        image: '/images/logo.jpg'
       },
       {
         name: '王师傅',
         experience: '从业12年',
         description: '专注长途运输，经验丰富',
-        image: '/images/driver3.jpg'
+        image: '/images/logo.jpg'
       }
     ])
 
